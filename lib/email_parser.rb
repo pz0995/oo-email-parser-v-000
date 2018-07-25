382f112ea6d1dd5a30f104a326a761ed395821a4
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-25 10:21:28 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,9 +10,9 @@ class EmailParser
 
 def extract_emails_to_array(text)
 
-  reg = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  # reg = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 @email_list.split(/[,\s]+/)
-  text.scan(reg).uniq
+  # text.scan(reg).uniq
 
 end
 
