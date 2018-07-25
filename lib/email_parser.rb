e08456988826c5425318c837e709695f75998337
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-25 12:35:50 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   attr_accessor :parse, :email_list, :emails
 
 def parse_emails
-email_list = emails.split(/\W+\s)
+email_list = emails.split(/\w+\s)
 
 # emails.strip!
 # emails.gsub!(/\r\n/,'\n')
